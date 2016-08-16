9f3f8e0806750852482f8870aa60443cdd262eb6
anget <anget@anget.com>
2016-08-17 00:03:43 +0300

    создана форма,база,начал валидацию

--- app/src/TestBundle/Controller/DefaultController.php
+++ app/src/TestBundle/Controller/DefaultController.php
@@ -4,14 +4,53 @@ namespace TestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use TestBundle\Entity\Feedback;
 
 class DefaultController extends Controller
 {
+//    /**
+//     * @Route("/")
+//     */
+//    public function indexAction()
+//    {
+//        return $this->render('TestBundle:Default:index.html.twig');
+//    }
+
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('TestBundle:Default:index.html.twig');
+        $feedback = new Feedback();
+
+        $form = $this->createFormBuilder($feedback)
+            ->setAction($this->generateUrl('test_default_index'))
+            ->add('name', null)
+            ->add('age', null)
+            ->add('email', null)
+            //->add('file', FileType::class)
+            ->add('save', SubmitType::class, array('label' => 'Submit'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $feedback = $form->getData();
+
+             $em = $this->getDoctrine()->getManager();
+             $em->persist($feedback);
+             $em->flush();
+
+            return $this->redirectToRoute('feedback_success'
+          //      , array('date' => $feedback)
+            );
+        }
+
+        return $this->render('TestBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ app/src/TestBundle/Entity/Feedback.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace TestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Feedback
+ *
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="feedback")
+ * @ORM\Entity(repositoryClass="TestBundle\Repository\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     ** @Assert\Range(
+     *      min = 17,
+     *      max = 65,)
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="File")
+     * @ORM\JoinColumn(name="file_id", referencedColumnName="id")
+     */
+    protected $file;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Feedback
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     * @return Feedback
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return integer 
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Feedback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function PrePersist()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @param mixed $file
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+}
